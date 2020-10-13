8a2527b18c8c89b16708dc6de7c110b2c85f9620
Diego Scafati <dscafati@gmail.com>
2020-10-14 00:21:32 +0200

    Added redirect function

---
+++ lambda/redirect.js
@@ -0,0 +1,12 @@
+exports.handler = async (event, context) => {
+  const redirectUrl = "https://nalathletics.com";
+  console.log("redirecting", event);
+  return {
+    statusCode: 302,
+    headers: {
+      Location: redirectUrl,
+      "Cache-Control": "no-cache"
+    },
+    body: JSON.stringify({})
+  };
+};
