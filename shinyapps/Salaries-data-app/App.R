cf4d338dce145481cefc3de1c5435e269526163a
GeorgeNAL <george.perry@austintrackclub.com>
2020-12-17 10:47:32 -0600

    Update App.R

--- shinyapps/Salaries-data-app/App.R
+++ shinyapps/Salaries-data-app/App.R
@@ -17,7 +17,7 @@ otheravg <- readRDS("data/otheravg.rds")
 
 ui <- fluidPage(
   
-  tags$head(includeCSS("www/Salaries-data-layout.css")),
+  tags$head(includeCSS("Salaries-data-layout.css")),
   titlePanel("Public university track & field coaches: Average salaries"),
   actionLink("DI", "NCAA Division I"),
   actionLink("DII", "NCAA Division II"),
