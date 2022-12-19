98f92fde4a019244531f57a0e8f41047e85a441c
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-12-18 22:49:22 -0600

    removed footer filler content and built out NALathletics footer structure and styling

--- senco-app/src/components/footer/footer-1.tsx
+++ senco-app/src/components/footer/footer-1.tsx
@@ -10,6 +10,7 @@ import {
   Container,
   DarkMode,
   Divider,
+  Flex,
   HStack,
   Input,
   Link,
@@ -88,131 +89,90 @@ const Footer1: React.FC<ChakraProps & ThemingProps> = ({
       pb={10}
       {...rest}
     >
-      <Container maxW={"container.xl"}>
-        <VStack spacing={10}>
-          <Stack
-            align={["center", "center", "start", "start"]}
-            direction={["column", "column", "row", "row"]}
-            width="full"
-            spacing={[8, 16, 20]}
-          >
-            <Stack direction="column" spacing={8} flex="1" align="stretch">
-              <DarkMode>
-                <NextLink href="/" passHref>
-                  <Link
-                    width="fit-content"
-                    _hover={{ color: "whiteAlpha.700" }}
-                  >
-                    <HStack align="center">
-                      <Logo />
-                    </HStack>
-                  </Link>
-                </NextLink>
-              </DarkMode>
-              <Text>{Site.slogan}</Text>
-              <Divider borderColor="gray.600" />
-              <VStack align="stretch" spacing={[4]}>
+      {/* Need to get the  */}
+      <Flex justifyContent='center'>
+          <Container maxW={"container.xl"}>
+            <VStack spacing={10}>
                 <Stack
-                  w="full"
-                  spacing={4}
-                  direction={["column", "column", "row"]}
+                  align={["center", "center", "start", "start"]}
+                  direction={["column", "column", "row", "row"]}
+                  width="full"
+                  spacing={[8, 16, 20]}
                 >
-                  <Input
-                    name="email"
-                    type="email"
-                    placeholder={"Enter your email"}
-                    borderColor={"gray.600"}
-                  />
-                  <Button flex="1 0 auto">Subscribe</Button>
-                </Stack>
-                <Text color={color} fontSize="sm">
-                  If you subscribe to our weekly newsletter, you will be
-                  informed about the developments regularly.
-                </Text>
-              </VStack>
-            </Stack>
-            <SimpleGrid w="full" flex="2" columns={[1, 2, 2, 3]} spacing={[10]}>
-              {FooterNavItems.map((item, idx) => (
-                <VStack
-                  key={`footer-nav-item-${item.title}-${idx}`}
-                  align="stretch"
-                  spacing={[2, 2, 4]}
-                >
-                  <Text fontWeight="semibold">{item.title}</Text>
-                  {item.subitems && (
-                    <List spacing={[2]}>
-                      {item.subitems.map((subitem, sidx) => (
-                        <ListItem
-                          key={`footer-nav-subitem-${item.title}-${subitem.title}-${sidx}`}
-                          {...linkStyle}
+                  <Stack direction="column" spacing={8} flex="1" align="stretch">
+                    <DarkMode>
+                      <NextLink href="https://nalathletics.com/blog" passHref>
+                        <Link
+                          width="fit-content"
+                          _hover={{ color: "whiteAlpha.700" }}
                         >
-                          {subitem.icon && <ListIcon as={subitem.icon} />}
-                          <NextLink href={subitem.href} passHref>
-                            <Link
-                              pos="relative"
-                              {...linkStyle}
-                              {...(isActive(subitem.href)
-                                ? activeLinkStyle
-                                : null)}
-                            >
-                              {subitem.title}
-                            </Link>
-                          </NextLink>
-                        </ListItem>
-                      ))}
-                    </List>
-                  )}
-                  {item.children}
-                </VStack>
-              ))}
-            </SimpleGrid>
-          </Stack>
+                          <HStack align="center">
+                            <Logo />
+                          </HStack>
+                        </Link>
+                      </NextLink>
+                    </DarkMode>
+                    <Text>{Site.slogan}</Text>
+                  </Stack>
+                  <SimpleGrid w="full" flex="2" columns={[1, 2, 2, 3]} spacing={[10]}>
+                    {FooterNavItems.map((item, idx) => (
+                      <VStack
+                        key={`footer-nav-item-${item.title}-${idx}`}
+                        align="stretch"
+                        spacing={[2, 2, 4]}
+                      >
+                        <Text fontWeight="semibold">{item.title}</Text>
+                        {item.subitems && (
+                          <List spacing={[2]}>
+                            {item.subitems.map((subitem, sidx) => (
+                              <ListItem
+                                key={`footer-nav-subitem-${item.title}-${subitem.title}-${sidx}`}
+                                {...linkStyle}
+                              >
+                                {subitem.icon && <ListIcon as={subitem.icon} />}
+                                <NextLink href={subitem.href} passHref>
+                                  <Link
+                                    pos="relative"
+                                    {...linkStyle}
+                                    {...(isActive(subitem.href)
+                                      ? activeLinkStyle
+                                      : null)}
+                                  >
+                                    {subitem.title}
+                                  </Link>
+                                </NextLink>
+                              </ListItem>
+                            ))}
+                          </List>
+                        )}
+                        {item.children}
+                      </VStack>
+                    ))}
+                  </SimpleGrid>
+                </Stack>
 
-          <Divider borderColor="gray.600" />
+                <Divider borderColor="gray.600" />
 
-          <Stack
-            direction={["column", "column", "row"]}
-            justify="space-between"
-            width="full"
-          >
-            <Stack align="center" direction={["column", "column", "row"]}>
-              <PrefersColorSchemeSelect />
-              <Text
-                fontSize="sm"
-                textAlign={["center", "center", "start", "start"]}
-              >
-                © All rights reserved. Senco LLC. Powered by Themebiotic.
-              </Text>
-            </Stack>
-            <FooterIcons />
-          </Stack>
-        </VStack>
-      </Container>
+                <Stack
+                  direction={["column", "column", "row"]}
+                  justify="space-between"
+                  width="full"
+                >
+                  <Stack align="center" direction={["column", "column", "row"]}>
+                    {/* <PrefersColorSchemeSelect /> */}
+                    <Text
+                      fontSize="sm"
+                      textAlign={["center", "center", "start", "start"]}
+                    >
+                      {/* Copyright © 2021 <link href="/blog">NALAthletics.</link> All Rights Reserved */}
+                      Copyright © 2021 <Link href="https://nalathletics.com/blog" >NALAthletics.</Link> All Rights Reserved
+                    </Text>
+                  </Stack>
+                </Stack>
+            </VStack>
+          </Container>
+      </Flex>
     </Box>
-  );
-};
-
-const FooterIcons = () => {
-  const items = [
-    { title: "VisaCard", icon: <VisaCardIcon /> },
-    { title: "MasterCard", icon: <MasterCardIcon /> },
-    { title: "AmexCard", icon: <AmexCardIcon /> },
-    { title: "ApplePay", icon: <ApplePayIcon /> },
-    { title: "PaypalPay", icon: <PaypalPayIcon /> },
-    { title: "DinersClub", icon: <DinersClubIcon /> },
-    { title: "Discover", icon: <DiscoverIcon /> },
-    { title: "FacebookPay", icon: <FacebookPayIcon /> },
-    { title: "GooglePay", icon: <GooglePayIcon /> },
-    { title: "ShopPay", icon: <ShopPayIcon /> },
-  ];
-  return (
-    <Wrap justify="center">
-      {items.map((item, idx) => (
-        <WrapItem key={`payment-icons-${item.title}-${idx}`}>
-          {item.icon}
-        </WrapItem>
-      ))}
-    </Wrap>
   );
 };
 
