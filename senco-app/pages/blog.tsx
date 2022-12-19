6cea3437f8aad715c5bd101baa74e587962a48d7
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-12-18 22:53:33 -0600

    removed old blog page and replaced with new tsx file

--- senco-app/pages/blog.tsx
+++ senco-app/pages/blog.tsx
@@ -1,8 +1,64 @@
-import React, { useContext, useState } from "react";
+import React, { memo, useContext, useRef, FC } from "react";
 
 import { NextSeo } from "next-seo";
-import NextLink from "next/link";
 
-// This will be the blog page -> 
-// Look in /src/components for header and footer
-// Will probably need to modify these components
+import {
+    AspectRatio,
+    Box,
+    Button,
+    chakra,
+    type ChakraProps,
+    Container,
+    Divider,
+    Flex,
+    Heading,
+    HStack,
+    Link,
+    SimpleGrid,
+    Stack,
+    Text,
+    type TextProps,
+    type ThemingProps,
+    useColorModeValue,
+    VStack,
+    Wrap,
+    WrapItem,
+  } from "@chakra-ui/react";
+
+import { ThemeColorContext } from "@definitions/context/theme";
+import { useCountdown } from "@definitions/hooks/useCountdown";
+
+import Image from "@components/image";
+import SubscribeInput from "@components/subscribe-input";
+import Hero1 from "@components/hero/hero-1";
+
+const HomePage: FC = () => {
+    const theme = useContext(ThemeColorContext);
+
+    const themeColorScheme = theme.colorScheme; // global default primary theme color
+
+    return(
+        <>
+        {/* https://github.com/garmeeh/next-seo#default-seo-configuration */}
+            <NextSeo
+                title="NALathletics blog"
+                description="NALathletics makes it easier to be an athlete or fan of athletics."
+            />
+            {/* Need to figure  */}
+            <chakra.main>
+                <Hero1 
+                    title='Blog' 
+                    body='' 
+                    bodyWidth={100} 
+                    backgroundImage='/images/blog_page_header.jpg' 
+                    backgroundPosition='center' 
+                    backgroundSize='cover'
+                />
+                <SubscribeInput />
+            </chakra.main>
+        </>
+
+    )
+};
+
+export default HomePage;
